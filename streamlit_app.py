e134c50403b3b032e6eaeabff05870dee8b8ba01
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-15 22:16:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.caption('<span style="font-size:30px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0Thanks for your subscription</span>', unsafe_allow_html=True)
+st.caption('<span style="font-size:45px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0Thanks for your subscription</span>', unsafe_allow_html=True)
 st.write("")
 
 col1, col2, col3 = st.columns(3)
 with col2:
-    st.image("tick.png", width=250)
+    st.image("tick.png", width=230)
 st.write("")
 st.write("")
 
@@ -24,6 +24,6 @@ st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\
 st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0Start picking the main \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 points</span>', unsafe_allow_html=True)
 st.subheader("")
 
-return_to_website = st.button("Let's go", type="primary", use_container_width=True)
+return_to_website = st.button("Let's go", type="primary")
 st.text("")
 st.image("pickiepoint_logo_profile_picture.jpg", width=200)
