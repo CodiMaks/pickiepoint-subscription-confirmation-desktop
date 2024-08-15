cef2cb3427ce0ff1c1170a4354ba7ad8d2b02d45
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-15 22:26:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,15 +17,15 @@ st.write("")
 col1, col2, col3 = st.columns(3)
 with col2:
     st.image("tick.png", width=230)
-st.write("")
-st.write("")
+    st.write("")
+    st.write("")
+
+st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0You are all set</span>', unsafe_allow_html=True)
+st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0Start picking the main points</span>', unsafe_allow_html=True)
+st.subheader("")
 
 sub_col1, sub_col2, sub_col3 = st.columns(3)
 with sub_col2:
-    st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0You are all set</span>', unsafe_allow_html=True)
-    st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0Start picking the main points</span>', unsafe_allow_html=True)
-    st.subheader("")
-
     return_to_website = st.button("Let's go", type="primary", use_container_width=True)
     st.text("")
     st.image("pickiepoint_logo_profile_picture.jpg", width=200)
