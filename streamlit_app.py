349fcda8fc25f44cd0d74fa7a028d33f6cc66ca1
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-15 22:18:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,8 @@ st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\
 st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0Start picking the main \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 points</span>', unsafe_allow_html=True)
 st.subheader("")
 
-return_to_website = st.button("Let's go", type="primary")
-st.text("")
-st.image("pickiepoint_logo_profile_picture.jpg", width=200)
+sub_col1, sub_col2, sub_col3 = st.columns(3)
+with sub_col2:
+    return_to_website = st.button("Let's go", type="primary")
+    st.text("")
+    st.image("pickiepoint_logo_profile_picture.jpg", width=200)
