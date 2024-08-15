db588bf835b33106a0307b491c49bb1461383b13
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-15 22:19:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,11 @@ st.write("")
 st.write("")
 
 st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0You are all set</span>', unsafe_allow_html=True)
-st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0Start picking the main \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 points</span>', unsafe_allow_html=True)
+st.caption('<span style="font-size:25px; font-weight:bold; color:white;">\u00A0\u00A0\u00A0\u00A0Start picking the main points</span>', unsafe_allow_html=True)
 st.subheader("")
 
 sub_col1, sub_col2, sub_col3 = st.columns(3)
 with sub_col2:
-    return_to_website = st.button("Let's go", type="primary")
+    return_to_website = st.button("Let's go", type="primary", use_column_width=True)
     st.text("")
     st.image("pickiepoint_logo_profile_picture.jpg", width=200)
