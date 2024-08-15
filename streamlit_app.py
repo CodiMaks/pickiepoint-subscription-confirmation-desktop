05f56bdae6e997af01921780b8e4aaf499244b57
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-15 22:01:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 st.caption('<span style="font-size:40px; font-weight:bold; color:white;">Thanks for your \u00A0\u00A0 subscription</span>', unsafe_allow_html=True)
 st.write("")
 
-st.image("accept.png", width=100)
+st.image("accept.png", width=300)
 st.write("")
 st.write("")
 
@@ -24,4 +24,4 @@ st.subheader("")
 
 return_to_website = st.button("Let's go", type="primary", use_container_width=True)
 st.text("")
-st.image("pickiepoint_logo_profile_picture.jpg", use_column_width=True)
+st.image("pickiepoint_logo_profile_picture.jpg", width=200)
